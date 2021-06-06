4e0fa4bbc508aced3802023287f1cc5af09e7748
suyog <suyoggonte@gmail.com>
2021-06-06 11:23:32 +0530

    Committing first change

--- SuyogWorkspace/src/com/suyog/java/corejava/firstProgram/HelloWorld.java
+++ SuyogWorkspace/src/com/suyog/java/corejava/firstProgram/HelloWorld.java
@@ -2,6 +2,6 @@ package com.suyog.java.corejava.firstProgram;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello World");
     }
 }
