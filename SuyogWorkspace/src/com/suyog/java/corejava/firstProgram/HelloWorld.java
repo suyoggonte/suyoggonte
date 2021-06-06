d352fe52d9011c54948c3569732de368530f0517
suyog <suyoggonte@gmail.com>
2021-06-06 12:43:13 +0530

    one more try of commit

--- SuyogWorkspace/src/com/suyog/java/corejava/firstProgram/HelloWorld.java
+++ SuyogWorkspace/src/com/suyog/java/corejava/firstProgram/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("First project committing to git hub repository");
+        System.out.println("One more try");
     }
 }
