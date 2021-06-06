7fa5ccbb8d31d3426e7c2816e3e14d27014dbca9
suyog <suyoggonte@gmail.com>
2021-06-06 12:39:43 +0530

    First commit

--- SuyogWorkspace/src/com/suyog/java/corejava/firstProgram/HelloWorld.java
+++ SuyogWorkspace/src/com/suyog/java/corejava/firstProgram/HelloWorld.java
@@ -3,5 +3,6 @@ package com.suyog.java.corejava.firstProgram;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("First project committing to git hub repository");
     }
 }
